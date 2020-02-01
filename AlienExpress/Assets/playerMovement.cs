464ab0697f79fef0f6e8192fcf02a33a7ee28cc0
Kaleth Cristancho <Kaleth_Joseh@hotmail.com>
2020-02-01 01:22:50 -0500

    SplaceHolders!
    Se añadieron Splaceholders de la escena escena de juego y de objetos random para empezar a programar.

--- AlienExpress/Assets/playerMovement.cs
+++ AlienExpress/Assets/playerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script1 : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     public float moveForce;         
     public float maxSpeed;     
