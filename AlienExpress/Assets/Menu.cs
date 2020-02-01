390280e40b7d89599fa4ce3282d639dae2e16c1d
KevinPicon01 <kevin.picon.2016@upb.edu.co>
2020-02-01 01:19:16 -0500

    Intro
    Se crea el intro

---
+++ AlienExpress/Assets/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public GameObject gameObject;
+    public string SceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            SceneManager.LoadScene(sceneName: SceneName);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+}
