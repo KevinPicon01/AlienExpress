5359702744272a473646fdcb47ca0eac527d8e45
KevinPicon01 <kevin.picon.2016@upb.edu.co>
2020-01-31 23:00:55 -0500

    Movimiento lateral
    La nave ya se mueve lateralmente

---
+++ AlienExpress/Assets/script1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float speed;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            _rigidbody.AddForce(Vector2.right * speed);
+        
+        if (Input.GetAxis("Horizontal") < 0 )
+            _rigidbody.AddForce(Vector2.left * speed);
+        
+        
+        
+
+
+    }
+}
