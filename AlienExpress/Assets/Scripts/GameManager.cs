d62760372a74cdfdeb7cddcb8296a6a45ffcc2e6
Kaleth Cristancho <Kaleth_Joseh@hotmail.com>
2020-02-01 15:42:01 -0500

    Randomizador added
    Se añadió el randomizador.

---
+++ AlienExpress/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+
+    
+    // 0 Antes 1 Durante 2 Despues
+    private int jugando = 0;
+    public int Jugando
+    {
+        get
+        {
+            return jugando;
+        }
+        set
+        {
+            jugando = value;
+        }
+    }
+
+    private GameObject[] spawnedObject = new GameObject[3];
+    public GameObject[] SpawnedObject
+    {
+        get
+        {
+            return spawnedObject;
+        }
+        set
+        {
+            spawnedObject = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        if (PlayerPrefs.GetString("potato").ToString().Length == 0)
+        {
+            PlayerPrefs.SetString("potato", "Pamery's God");
+        }
+    }
+
+}
--- AlienExpress/Assets/Scripts/RandomObjects.cs
+++ AlienExpress/Assets/Scripts/RandomObjects.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RandomObjects : MonoBehaviour
 {
+    public GameObject[] objs = new GameObject[5];
+
+    Random rand;
+
+    GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
+        gm = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+        ShuffleObjects(objs);
         
+    
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(objs[i], new Vector3(i * 1.5f, 0, 0), Quaternion.identity);
+        }
+        //rand = Random(objs.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(gm.SpawnedObject);
     }
+
+    
+    void ShuffleObjects(GameObject[] ob)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < ob.Length; t++)
+        {
+            GameObject tmp = ob[t];
+            int r = Random.Range(t, ob.Length);
+            ob[t] = ob[r];
+            ob[r] = tmp;
+            print(ob[t]);
+        }
+    }
+    
 }
