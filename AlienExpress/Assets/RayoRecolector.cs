3b4b34b30c5c03fe99dbd2654ceae17c56516e4c
jhankk <57294300+jhankk@users.noreply.github.com>
2020-02-02 10:34:54 -0500

    Postprocesing
    cambios visuales

---
+++ AlienExpress/Assets/RayoRecolector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayoRecolector : MonoBehaviour
+{
+    public Transform Player;
+
+    public GameObject bull;
+    //public Transform Rope;
+
+    //private GameObject HookChild;
+    //public Transform RopeChild;
+
+    void Update()
+    {
+        Ray HookRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(HookRay.origin, HookRay.direction, Color.green);
+        RaycastHit HookHit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if (Physics.Raycast(HookRay, out HookHit))
+            {
+                Debug.DrawRay(HookRay.origin, HookRay.direction, Color.red);
+                
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+         
+        }
+    }
+}
